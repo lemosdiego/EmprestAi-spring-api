a97dc58e3b137ab51356f1c59b26eb292b354eef
Washington <washington.contato1327@gmail.com>
2026-02-27 10:23:49 -0300

    Primeiro commit

---
+++ src/main/java/br/com/emprestai/EmprestAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.emprestai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/emprestai/EmprestAiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emprestai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprestAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
