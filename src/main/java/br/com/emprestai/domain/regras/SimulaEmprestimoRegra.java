1c8e4b958f1020e463d30ce1d577b9d8983c483e
Washington <washington.contato1327@gmail.com>
2026-02-27 14:53:23 -0300

    feat(domain) Adiciona regra de negocio para Simular um emprestimo com base no salario liquido

---
+++ src/main/java/br/com/emprestai/domain/regras/SimulaEmprestimoRegra.java
@@ -0,0 +1,19 @@
+package br.com.emprestai.domain.regras;
+
+import br.com.emprestai.domain.entity.simulacao.Simulacao;
+import br.com.emprestai.domain.enums.emprestimo.FaixaCredito;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class SimulaEmprestimoRegra {
+    public Simulacao run(BigDecimal salarioLiquido, Integer parcelas){
+        FaixaCredito regra = FaixaCredito.FaixaPara(salarioLiquido);
+
+        BigDecimal limite = regra.calcularLimite(salarioLiquido);
+        BigDecimal valorParcelas = regra.calcularParcelas(limite, parcelas);
+        BigDecimal porcentagemSalario = valorParcelas.divide(salarioLiquido, 4, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100));
+
+        return new Simulacao(null, salarioLiquido, regra.name(), limite, parcelas, valorParcelas, porcentagemSalario);
+    }
+}
