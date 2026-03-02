54a7b2da05bd742ebc93c3059ad4bcef1cb689cb
Washington <washington.contato1327@gmail.com>
2026-03-02 13:24:37 -0300

    feat(application):Cria e ajusta controller

--- src/main/java/br/com/emprestai/application/controllers/SimularEmprestimoController.java
+++ src/main/java/br/com/emprestai/application/controllers/SimularEmprestimoController.java
@@ -1,4 +1,25 @@
-package br.com.emprestai.application.controllers.dto;
+package br.com.emprestai.application.controllers;
 
+
+import br.com.emprestai.application.controllers.dto.request.SimularEmprestimoRequest;
+import br.com.emprestai.application.controllers.dto.response.SimularEmprestimoResponse;
+import br.com.emprestai.application.usecases.SimularEmprestimoUseCase;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/simulacoes")
 public class SimularEmprestimoController {
+    private final SimularEmprestimoUseCase useCase;
+
+    public SimularEmprestimoController(SimularEmprestimoUseCase useCase){
+        this.useCase = useCase;
+    }
+    @PostMapping
+    public SimularEmprestimoResponse simulate(@RequestBody SimularEmprestimoRequest request){
+        return useCase.process(request);
+    }
 }
